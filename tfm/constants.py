2c254c930b35698c9f7a007c2a8fd955b14abd2a
carpelli <92866289+carpelli@users.noreply.github.com>
2022-12-28 14:42:57 +0100

    run with maximportance sampler on azure

--- tfm/constants.py
+++ tfm/constants.py
@@ -6,6 +6,6 @@ import samplers
 DATA_SAMPLE_SIZE = 2000
 UPPER_DIM = 1
 OUTDIR = Path(__file__).parent / '../out'
-SAMPLER = samplers.Random(3000)
+SAMPLER = samplers.MaxImportance(3000)
 # SAMPLER = samplers.StratifiedRandom(3000)
 TIMEOUT = 1000
