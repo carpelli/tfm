f01a237a53f028a65de2722e205a9152651a8d2f
carpelli <92866289+carpelli@users.noreply.github.com>
2022-12-28 17:40:27 +0100

    run avg importance sampler on azure

--- tfm/constants.py
+++ tfm/constants.py
@@ -6,6 +6,5 @@ import samplers
 DATA_SAMPLE_SIZE = 2000
 UPPER_DIM = 1
 OUTDIR = Path(__file__).parent / '../out'
-SAMPLER = samplers.MaxImportance(3000)
-# SAMPLER = samplers.StratifiedRandom(3000)
+SAMPLER = samplers.AvgImportance(3000)
 TIMEOUT = 1000
